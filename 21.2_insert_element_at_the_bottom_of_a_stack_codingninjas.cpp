f1aa6268e3d70af7f26d42d3ecab261444fa1885
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2022-11-19 21:41:35 +0600

    ds, stack, recursion

--- 21.2_insert_element_at_the_bottom_of_a_stack_codingninjas.cpp
+++ 21.2_insert_element_at_the_bottom_of_a_stack_codingninjas.cpp
@@ -1,18 +1,21 @@
-void rec(stack<int>& myStack, int i, int n, int x) {
-    if (i == n) {
+#include <bits/stdc++.h> 
+
+void rec(stack<int>& myStack, int x) {
+    if (myStack.empty()) {
         myStack.push(x);
         return;
     }
     int top = myStack.top();
     myStack.pop();
-    rec(myStack, i + 1, n, x);
+    rec(myStack, x);
     myStack.push(top);
 }
 
 stack<int> pushAtBottom(stack<int>& myStack, int x) 
 {
     int n = myStack.size();
-    rec(myStack, 0, n, x);
+    rec(myStack, x);
     
     return myStack;
 }
+
