89b78000faefc6b885ad1e682e64f4d2381a9a92
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2022-11-19 14:18:11 +0600

    backtracking, recursion, dfs

--- 13.1_rat_in_a_maze.cpp
+++ 13.1_rat_in_a_maze.cpp
@@ -1,36 +1,54 @@
-//{ Driver Code Starts
-// Initial Template for C
-
-#include <stdio.h>
-#include <stdbool.h>
-
-
-// } Driver Code Ends
-// User function Template for C
-
-bool arraySortedOrNot(int arr[], int n) {
-
-    if (n < 2) {
-        return true;
+class Solution{https://practice.geeksforgeeks.org/explore
+    public:
+    bool isValid(vector<vector<int>> &m, int n, vector<vector<int>> vis, int x, int y) {
+        return ((x >= 0 && x < n) && (y >= 0 && y < n)) && m[x][y] == 1 && vis[x][y] == 0;
     }
-    return (arr[n - 1] >= arr[n - 2]) && arraySortedOrNot(arr, n - 1);
-}
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    scanf("%d", &t);
-    while (t--) {
-        int n;
-        scanf("%d", &n);
-        int arr[n];
-        for (int i = 0; i < n; i++) {
-            scanf("%d", &arr[i]);
+    void dfs(vector<vector<int>> &m, int n, vector<vector<int>>& vis, int x, int y, string path, vector<string>& ans) {
+        if (x == n - 1 && y == n - 1) { // reached the destination
+            ans.push_back(path); // push path to the answer
+            return;
         }
-        bool ans = arraySortedOrNot(arr, n);
-        printf("%d\n", ans);
+        
+        vis[x][y] = 1; // mark the current cell visited
+        int dx, dy;
+        
+        // go left
+        if (isValid(m, n, vis, x, y - 1)) { // check the next move is valid or not
+            dfs(m, n, vis, x, y - 1, path + 'L', ans); // call the next move with updated path
+        }
+        
+        // go up
+        if (isValid(m, n, vis, x - 1, y)) { // check the next move is valid or not
+            dfs(m, n, vis, x - 1, y, path + 'U', ans); // call the next move with updated path
+        }
+        
+        // go right
+        if (isValid(m, n, vis, x, y + 1)) { // check the next move is valid or not
+            dfs(m, n, vis, x, y + 1, path + 'R', ans); // call the next move with updated path
+        }
+        
+        // go down
+        if (isValid(m, n, vis, x + 1, y)) { // check the next move is valid or not
+            dfs(m, n, vis, x + 1, y, path + 'D', ans); // call the next move with updated path
+        }
+        
+        vis[x][y] = 0;
     }
-    return 0;
-}
-// } Driver Code Ends
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        vector<vector<int>> vis = m;
+        vector<string> ans;
+        
+        if (m[0][0] == 0) {
+            return ans;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                vis[i][j] = 0;
+            }
+        }
+        dfs(m, n, vis, 0, 0, "", ans);
+       
+        return ans;
+    }
+};
+
